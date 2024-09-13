48e8ee37f5d7aba1679033dc31f1b6cce03b3c9e
thedastan <dastanshermatov@gmail.com>
2024-09-13 17:08:00 +0600

    улучшение

--- src/constants/admin.ts
+++ src/constants/admin.ts
@@ -1,9 +1,9 @@
-export const PHONE_NUMBER = '+996707759039'
+export const PHONE_NUMBER = '+996559692626'
 
 export const WHATSAPP_NUMBER = PHONE_NUMBER
 
-export const EMAIL_ADDRESS = 'tezcargotracker@gmail.com'
-export const INSTAGRAM = 'tezcargo24'
+export const EMAIL_ADDRESS = 'tezkyzmat.kg@gmail.com'
+export const INSTAGRAM = 'tez_kyzmat'
 
 // export const LOCATION = 'Бишкек, Турусбекова 109/3 БЦ Maximum+'
 
@@ -15,7 +15,6 @@ export const EMAIL_ADDRESS_LINK = `mailto:${EMAIL_ADDRESS}`
 
 export const INSTAGRAM_LINK = `https://instagram.com/${INSTAGRAM}`
 
-export const TELEGRAM_LINK = `https://t.me/+LzRB9C_dM29hNjdi`
 
 // export const LOCATION_LINK = 'https://go.2gis.com/5qc746'
 
